42dbaa79b75d5d87773e7bbef560327b1c2c5869
Lucas Ambrust <ambrust.lucas@gmail.com>
2020-02-02 17:41:17 -0300

    Primeiros passos com Javascript

---
+++ calculoAposentadoria.js
@@ -0,0 +1,19 @@
+const nome = 'Jéssica'
+const sexo = 'M'
+const idade = 60
+const contribuicao = 35
+
+if(sexo === 'F'){
+    if(contribuicao >= 30 && (idade + contribuicao >= 85)){
+        console.log(`${nome}, você pode se aposentar`)
+    }else {
+        console.log(`${nome}, você ainda não pode se aposentar.`)
+    }
+}
+if(sexo === 'M'){
+    if(contribuicao >=35 && (idade + contribuicao >= 95)){
+        console.log(`${nome}, você pode se aposentar`)
+    }else{ 
+        console.log(`${nome}, você ainda não pode se aposentar.`)
+    }
+}
